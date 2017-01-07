cd11d4350c919d5f83307eee8cda319026bcfba1
thisisayush <thisisayushaa@gmail.com>
2017-01-07 20:00:29 +0530

    Enhanced EventBox and added Info and other details
    - Upcoming Events Updated
    - Event Box Template Updated
    - Date added in Event Box
    - Why should I Join the club changed
    - Styling

--- js/data.js
+++ js/data.js
@@ -11,16 +11,39 @@ var services = [];
 var events = [];
 var alumni = [];
 
-/** Services List **/
-services[0] = "Test Service 1";
-services[1] = "Test Service 2";
-services[3] = "Test Service 3";
+/** Services List 
+	Do you want to stand out of crowd and learn unique skills?
+	Do you want to participate in Google Summer of code?
+	Do you want to be a part of hacker culture?
+	Do you have fun while learning?
+	Do you want  to learn trending technologies ? 
+	Do you want learn Linux and peripheral technologies ?
+	
+	Credits: Ajay Tripathi
+**/
 
-/** Events List **/
-events[0] = {title: "Test Event 1", cta: "#", ctaText: "Register Now", description: "Some description of this event! <strong> Can use Html Tags</strong>", startTime: "11:00 AM", endTime: "2:00 PM", location: "E3-316", eventOD: 1};
-events[1] = {title: "Test Event 2", cta: "#", ctaText: "Call To Action", description: "Some description of this event! <strong> Can use Html Tags</strong>", startTime: "11:00 AM", endTime: "2:00 PM", location: "Amity Noida", eventOD: 0};
-events[2] = {title: "Test Event 3", cta: "#", ctaText: "Register Now", description: "Some description of this event! <strong> Can use Html Tags</strong>", startTime: "11:00 AM", endTime: "2:00 PM", location: "E3-316", eventOD: 1};
-events[3] = {title: "Test Event 4", cta: "#", ctaText: "Register Now", description: "Some description of this event! <strong> Can use Html Tags</strong>", startTime: "11:00 AM", endTime: "2:00 PM", location: "E3-316", eventOD: 1};
+services[0] = "Stand out of crowd and learn unique skills from people already working in the industry!";
+services[1] = "Be a part of Hacker Culture";
+services[3] = "Learn by Fun and Collaboration";
+services[4] = "Learn Linux, Android, Networking, etc. There is no limit on how much and what you can learn!";
+services[5] = "Already know something? Teach it to others! That's how we work!";
+
+/** Events List 
+	title: Event Title
+	cta: Link url for Call to Action Button
+	ctaText: Call to Action Button Text
+	description: Event Description. Can Contain HTML Tags
+	date: Event Date
+	startTime: Event Start Time
+	endTime: Event End Time
+	location: Event Location
+	eventOD: 0 for NO, 1 for YES. The text and respective classes are automatically added by the script
+**/
+events[0] = {title: "Workshop in Kivy", cta: "#", ctaText: "Register Now", description: "Learn Kivy! ", date: "12 Jan '17", startTime: "NA", endTime: "NA", location: "NA", eventOD: 0};
+events[1] = {title: "Workshop in Python", cta: "#", ctaText: "Register Now", description: "Learn Python!", date:"Jan '17", startTime: "NA", endTime: "NA", location: "Amity Noida", eventOD: 0};
+events[2] = {title: "Workshop in Firefox", cta: "#", ctaText: "Register Now", description: "Learn Firefox!", date: "Jan '17", startTime: "11:00 AM", endTime: "2:00 PM", location: "E3-316", eventOD: 1};
+events[3] = {title: "PyDelhi", cta: "#", ctaText: "Register Now", description: "PyDelhi: Details Coming Soon", startTime: "NA", date: "NA", endTime: "NA", location: "NA", eventOD: 0};
+events[4] = {title: "ILUG D", cta: "#", ctaText: "Register Now", description: "ILUG D: Details Coming Soon", startTime: "NA", date: "NA", endTime: "NA", location: "NA", eventOD: 0};
 /** End Events List **/
 
 /**Alumni List **/
@@ -57,11 +80,12 @@ for(var x in services){
 
 /** Generate Event Boxes **/
 for (var x in events){
-		var html = '<div class="col-lg-3 eventBox col-centered"><div class="row eventTitle"><div class="col-lg-12 text-center"><h2>'+events[x].title+'</h2></div></div><div class="row eventInfo"><div class="col-lg-7 eventDesc"><div class="row eventDescInfo"><div class="col-lg-12"><p class="eventDescInfoContainer">'+events[x].description+'</p></div><div class="row eventDescCTA"><div class="col-lg-12"><a href="'+events[x].cta+'"><button class="btn-primary">'+events[x].ctaText+'</button></a></div></div></div></div><div class="col-lg-5 eventTiming text-left"><div class="startTimimg"><i class="fa fa-clock-o"></i> <span>'+events[x].startTime+'</span></div><div class="endTimimg"><i class="fa fa-clock-o"></i> <span>'+events[x].endTime+'</span></div><div class="eventLocation"><i class="fa fa-map-marker"></i> <span>'+events[x].location+'</span></div><div class="eventOD"><strong>OD</strong> <span class="';
+		var html = '<div class="col-lg-3 eventBox col-centered text-center"><div class="row eventTitle"><div class="col-lg-12 text-center"><h2>'+events[x].title+'</h2></div></div><div class="row eventInfo"><div class="col-lg-7 eventDesc"><div class="row eventDescInfo"><div class="col-lg-12"><p class="eventDescInfoContainer">'+events[x].description+'</p></div></div></div><div class="col-lg-5 eventTiming text-left"><div class="date"><i class="fa fa-calendar"></i> <span>'+events[x].date+'</span></div><div class="startTimimg"><i class="fa fa-clock-o"></i> <span>'+events[x].startTime+'</span></div><div class="endTimimg"><i class="fa fa-clock-o"></i> <span>'+events[x].endTime+'</span></div><div class="eventLocation"><i class="fa fa-map-marker"></i> <span>'+events[x].location+'</span></div><div class="eventOD"><strong>OD</strong> <span class="';
 		html += (events[x].eventOD)?"text-success":"text-danger";
 		html += '">';
 		html += (events[x].eventOD)?"Yes":"No";
-		html += '</span></div></div></div></div>';
+		html += '</span></div></div></div>';
+		html +='<div class="row eventCTA"><div class="col-lg-12"><a href="'+events[x].cta+'"><button class="btn-primary">'+events[x].ctaText+'</button></a></div></div></div>';
 		eventsListContainer.append(html);
 }
 
