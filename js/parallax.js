f8f4850b97a1eba74ccafda427668a4615750b27
Animesh Verma <aniverma10@gmail.com>
2017-03-14 03:37:54 +0530

    Parallax script error fix

--- js/parallax.js
+++ js/parallax.js
@@ -4,5 +4,5 @@ $(document).ready(function(){
 $(window).scroll(function() {
   $("#home").css("transform","translate3d(0," +  (($(window).scrollTop())/2)  + "px,-1px)");   //Applying Y-translation to home section
 });
-
+});
 
