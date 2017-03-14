8b7a8885f6dece3379cb403437f366de0755aa37
thisisayush <thisisayushaa@gmail.com>
2017-03-14 19:11:28 +0530

    Fixed Color Scheme and Various other fixes - Fixed the color scheme to Blue untill better theme is developed. - Renamed Media Partner to Photography Partner - Restored ALiAS Logo in Tablet view and fixed menu in Tablet View - Fixed Preloader displaying before page is loaded!

--- js/Functions.js
+++ js/Functions.js
@@ -37,31 +37,32 @@ flashBoxCloseBtn.click(function(e){
 
 /**Button Random Color**/
 var colors = ['Amber','Lime','Green','Light-Blue','Blue','Purple','Red','Pink','Deep-Orange','color-1','color-2','color-3','color-4','color-5','color-6','color-7','color-8','color-9','color-10','color-11','color-12','color-13'];//Add color name from the colors.css file in this box to add/remove it from random function. 
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+var random_color = 'Blue';
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#contact .section-header").addClass("button-"+random_color);
 $(".eventCTA button").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#events-color-fix2").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#alumni .section-header").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#team .section-header").addClass("button-"+random_color);
 $(".CommonSectionbtn").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#joinNowBtn").addClass("button-"+random_color);
 $(".teamBox").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#events-color-fix").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#eventsBtn-color-fix1").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#eventsBtn-color-fix2").addClass("button-"+random_color);
 $("#rankerBtn").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#utilities-color-fix2").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#utilities-color-fix").addClass("button-"+random_color);
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 $("#utilities-color-fix3").addClass("button-"+random_color);
 
 $(".PositionPC").addClass("col-lg-2"); // Fix for contact us icon placing to avoid any placing error in tablet view.
