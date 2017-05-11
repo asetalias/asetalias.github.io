3401b6e8b5329cbf424b6b7159cac808738543c2
thisisayush <thisisayushaa@gmail.com>
2017-05-11 10:43:49 +0530

    (update) Introduction and Implementation of JSON
    (add) Introducing JSON Data Files
    - Introducing JSON Data Files instead of data.js
    - JSON will help in integrating website to dynamic backend in future plans (if any)
    
    (add) Introducing loadData(type)
    - loadData(type) function loads data from JSON Files
    - type can be "meta", "home", "communities", "events", "webinars"
    - Triggers various events "aliasMetaDataLoaded", "aliasHomeDataLoaded", etc when data is loaded in respective variables of js/data.js
    
    (update) Update flashbox code
    - Updated Flashbox code to make it compataible with new flashbox object
    - Improved checkFlashBox() code
    
    (Update) Updated Code to implement JSON Data
    - Updated Code on scripts to trigger codes when event Triggers (from loadData(type))
    
    (update) Emptied js/data.js
    - Removed data from js/data.js
    
    (update) Updated Team
    - Updated Team Details
    - Updated Team Box Generation Module
    - Added Different data fields for Github and Designation
    - Designation is given priority if both are provided
    
    (add) Added Shilpi Mam's Photo
    - Added Shilpi Mam's PPIC
    
    (fix) Fixed data/events.json
    
    (fix) Logo Url Typo
    
    (fix) Fixed Flashbox layout issue

--- js/main.js
+++ js/main.js
@@ -1,50 +1,59 @@
 $(document).ready(function(e){
+
     /** Load Data **/
-    document.title = siteTitle;			//Update Site Title
-    logoContainer.attr("src",logoUrl);  //Update Logo Url
+    loadData("meta");
+    $(document).on("aliasMetaDataLoaded", function(){
+        
+        document.title = siteTitle;			//Update Site Title
+        
+        logoContainer.attr("src",logoUrl);  //Update Logo Url
+        
+        generateContactLinks();
+        
+        /** Check for any FlashBox Entry **/
+	    checkFlashBox();
     
-    generateContactLinks();
+        $(window).resize(function(e){
+    		checkCollapseMenu();
+	    });
 
-    $(window).resize(function(e){
-		checkCollapseMenu();
-	});
-
-    $("#mainHeader li a").click(function(e){
-         $("a.active").removeClass("active");
-         $('a[href="index.html#'+$(this).attr("href").replace("index.html#","")+'"').addClass("active");
-    });
-    //jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function() {
-        $('a.page-scroll').bind('click', function(event) {
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href').replace("index.html","")).offset().top -90
-            }, 1500, 'easeInOutExpo');
+        $("#mainHeader li a").click(function(e){
+            $("a.active").removeClass("active");
+            $('a[href="index.html#'+$(this).attr("href").replace("index.html#","")+'"').addClass("active");
         });
-    });
-    $(document).ready(function(e){
-        $(".preloader").fadeOut(200);  //hide preloader
-        var isTrans = 0;    //for menu bar
-        var stickyMenu = function(){
-            var mainHeader = $("header#mainHeader");
-            if(mainHeader.hasClass("mainHeaderLayout1")){
-                isTrans = 1;
-            }
-            if(!isTrans){
-                return false;
-            }
-            setSticky();
-            $(window).scroll(function(e){
-                    setSticky();
+        //jQuery for page scrolling feature - requires jQuery Easing plugin
+        $(function() {
+            $('a.page-scroll').bind('click', function(event) {
+                var anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $(anchor.attr('href').replace("index.html","")).offset().top -90
+                }, 1500, 'easeInOutExpo');
             });
-            function setSticky(){
-                var h = mainHeader.height();
-                if($(window).scrollTop()+10>h){
-                    mainHeader.removeClass("mainHeaderLayout1");
-                }else{
-                    mainHeader.addClass("mainHeaderLayout1");
+        });
+        $(document).ready(function(e){
+            var isTrans = 0;    //for menu bar
+            var stickyMenu = function(){
+                var mainHeader = $("header#mainHeader");
+                if(mainHeader.hasClass("mainHeaderLayout1")){
+                    isTrans = 1;
                 }
-            }
-        }();
-    });  
+                if(!isTrans){
+                    return false;
+                }
+                setSticky();
+                $(window).scroll(function(e){
+                        setSticky();
+                });
+                function setSticky(){
+                    var h = mainHeader.height();
+                    if($(window).scrollTop()+10>h){
+                        mainHeader.removeClass("mainHeaderLayout1");
+                    }else{
+                        mainHeader.addClass("mainHeaderLayout1");
+                    }
+                }
+            }();
+            $(".preloader").fadeOut(200);  //hide preloader
+        });  
+    });
 });
