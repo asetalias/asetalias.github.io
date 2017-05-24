c9d3cfe2afb7072df675486f3f6f91be3ec0674e
thisisayush <thisisayushaa@gmail.com>
2017-05-24 16:30:42 +0530

    (fix) Preloader | MinScreenSizeOverlay
    (fix) Preloader
    - Fixed Preloader disappearing before complete page loads
    - Fixed Preloader logo getting replaced by Main Logo
    - Updated Preloader Cnntent Fonts and Logo Animation
    
    (fix) Min Screen Size Overlay Logo Fix
    - Relaced logo-square with logo-med in Min Screen Size Overlay

--- js/template/loader.js
+++ js/template/loader.js
@@ -1,5 +1,5 @@
 var html = '<div class="preloader">'+
                 '<div class="content">'+
-        	        '<div class="preloader-content"><img src="images/ALiAS.png" class="logo" style="width:150px;"><br>LOADING</div>'+
+        	        '<div class="preloader-content"><img src="images/ALiAS.png" class="logo-med animated-pulse"><br><span class="loading-text">loading</span></div>'+
                 '</div></div>';
 $("#contentWrap").prepend(html);
