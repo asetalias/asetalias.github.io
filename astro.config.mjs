1423da18ca27b873f5daa5365a0806493f6d077c
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-01-14 20:25:42 +0530

    build: add tailwind (#247)
    * dev: switch to pnpm
    
    * build: add tailwind
    
    * fmt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), react(), sitemap()],
+  integrations: [mdx(), react(), sitemap(), tailwind()],
 });
