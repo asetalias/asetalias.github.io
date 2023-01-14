1de89f0536bfecf7e4b91ec55b5c20c4e96c9145
Ashwin Kumar Yadav <90348893+ashwinky@users.noreply.github.com>
2023-01-14 22:17:47 +0530

    feat: add events section (#249)
    Co-authored-by: Maximous Black <maximousblk@gmail.com>
    Co-authored-by: Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>

---
+++ src/components/Events.svelte
@@ -0,0 +1,33 @@
+<script>
+  let currentSlide = 0;
+  import events from "@/data/events.json";
+</script>
+
+<div>
+  <h3>Upcoming Events</h3>
+  <button
+    id="previous"
+    on:click={() => {
+      currentSlide = (currentSlide - 1 + events.length) % events.length;
+    }}>Previous</button
+  >
+  <div class="carousel-container">
+    {#each events as event, i}
+      <div class={"slide " + (currentSlide === i ? "block" : "hidden")}>
+        <img src={event.poster} alt="img" />
+        <div>
+          <p>{event.title}</p>
+          <p>{new Date(event.date).toLocaleDateString()}</p>
+          <p>{event.location}</p>
+          <button>Add to Calender</button>
+        </div>
+      </div>
+    {/each}
+  </div>
+  <button
+    id="next"
+    on:click={() => {
+      currentSlide = (currentSlide + 1) % events.length;
+    }}>Next</button
+  >
+</div>
