bbbc30bac6e03e4fb9811c62d84007035e6ad587
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-01-14 21:23:20 +0530

    dev: add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,10 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), react(), sitemap(), tailwind()],
+  integrations: [mdx(), react(), sitemap(), tailwind(), svelte()]
 });
