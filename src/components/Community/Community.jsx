2790f331867466b31751419d8cf932ab3206995e
Shivam Shandilya <shiv.05cs@gmail.com>
2023-11-26 08:09:26 +0530

    fix(community): fix rustdelhi and pydelhi hyperlinks

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -12,14 +12,14 @@ const Community = () => {
         <span className={styles.community__title_highlight}> Community</span> Partners!
       </h1>
       <div className={styles.powered}>
-        <a href="https://pydelhi.in" target="_blank" rel="no noreferrer">
+        <a href="https://pydelhi.org" target="_blank" rel="no noreferrer">
           <img
             className={`${styles.community__chapter} ${styles.outline}`}
             src="assets/images/communities/pydelhi.png"
             alt="Pydelhi"
           />
         </a>
-        <a href="https://rustdlehi.in" target="_blank" rel="no noreferrer">
+        <a href="https://rustdelhi.in" target="_blank" rel="no noreferrer">
           <img
             className={styles.community__chapter}
             src="assets/images/communities/rustdelhi.png"
